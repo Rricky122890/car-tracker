02379dcaa87424fe4f45fcf93eea552c333aa75b
Rricky122890 <rjeanpierre90@gmail.com>
2025-03-21 15:45:25 -0400

    Updated sidebar to have sliding drawer

--- car-tracker-fe/src/components/SideBar.jsx
+++ car-tracker-fe/src/components/SideBar.jsx
@@ -1,10 +1,25 @@
 import { Drawer } from '@mui/material';
+import { use } from 'react';
+import { useState } from 'react';
 
 export default function SideBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  function toggleDrawer() {
+    setIsOpen((prevState) => !prevState);
+  }
   return (
     <div>
-      <h1>Hello</h1>
-      <Drawer />
+      <button onClick={toggleDrawer} style={{ marginTop: '46px' }}>
+        R
+      </button>
+      <Drawer onClose={toggleDrawer} open={isOpen}>
+        <h1 onClick={toggleDrawer}>Side Nav</h1>
+        <ul>
+          <p>Ricardo</p>
+          <p>20 Cars</p>
+          <p>Log Out</p>
+        </ul>
+      </Drawer>
     </div>
   );
 }
